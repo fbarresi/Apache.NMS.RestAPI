1770933af19c769afedd0312bebaacaa2ce8684e
Federico Barresi <federico.barresi@ams-osram.com>
2024-10-11 07:52:03 +0200

    removed default values

--- Apache.NMS.RestAPI.Interfaces/Settings/MessageBusSettings.cs
+++ Apache.NMS.RestAPI.Interfaces/Settings/MessageBusSettings.cs
@@ -2,9 +2,9 @@
 {
     public class MessageBusSettings
     {
-        public string ServerUrl { get; set; } = "ems:tcp://EMSAP-RBG01.germany.osram-os.com:7222";
-        public string Username { get; set; } = "admin";
-        public string Password { get; set; } = "";
-        public string Destination { get; set; } = "queue://abc";
+        public string ServerUrl { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Destination { get; set; }
     }
 }
